38bb825f3b9eaef8d400917f5e11f7d9b9822f37
Alexander Martinz <amartinz@shiftphones.com>
2022-01-07 17:16:46 +0100

    sdm845: add new "axolotl" target
    This adds initial support for the SHIFT6mq (axolotl).
    It launched with Android 10, makes use of dynamic partitions
    and has A/B.
    
    A/B is ignored in this bringup and will be added at a later
    stage.
    
    Change-Id: I88d42b009769a400b1f240d4e86018c0a7d90adb
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

---
+++ axolotl/device.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_COPY_FILES := \
+    $(LOCAL_PATH)/fstab.ramdisk:$(TARGET_COPY_OUT_RAMDISK)/fstab.axolotl \
+    $(LOCAL_PATH)/fstab.ramdisk:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.axolotl \
+    device/generic/sdm845/init.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.axolotl.rc \
+    device/generic/sdm845/init.common.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.axolotl.usb.rc \
+    device/generic/sdm845/common.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/axolotl.kl
+
+# Audio
+PRODUCT_PACKAGES += audio.primary.axolotl
+
+# Lights
+PRODUCT_PACKAGES += lights.axolotl
+
+# Dynamic Partitions
+PRODUCT_BUILD_SUPER_PARTITION := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
