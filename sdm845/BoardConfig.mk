bedffb53678a1f38d24ccf63aae523ac994d59d5
Alexander Martinz <amartinz@shiftphones.com>
2022-01-10 17:05:19 +0100

    sdm845: commonize BoardConfigs
    As beryllium and enchilada can already boot using the same images,
    unify the kernel and system image configuration.
    
    Change-Id: I2458e11959bbb88e91c233df7e6f613839d74bc3
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- sdm845/BoardConfig.mk
+++ sdm845/BoardConfig.mk
@@ -4,20 +4,3 @@ include device/generic/sdm845/BoardConfigCommon.mk
 # Board Information
 TARGET_BOOTLOADER_BOARD_NAME := sdm845
 #TARGET_BOARD_PLATFORM := sdm845
-
-# Kernel/boot.img Configuration
-BOARD_KERNEL_BASE        := 0x00000000
-BOARD_KERNEL_PAGESIZE    := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET     := 0x01000000
-BOARD_KERNEL_CMDLINE     := console=ttyMSM0,115200n8 earlycon earlyprintk console=tty0 androidboot.console=ttyMSM0
-BOARD_KERNEL_CMDLINE     += androidboot.hardware=sdm845 androidboot.boot_devices=soc@0/1d84000.ufshc
-BOARD_KERNEL_CMDLINE     += firmware_class.path=/vendor/firmware/ init=/init printk.devkmsg=on
-BOARD_KERNEL_CMDLINE     += deferred_probe_timeout=30
-
-# Image Configuration
-BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864 #64M
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2998927360
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 57453555712
-BOARD_VENDORIMAGE_PARTITION_SIZE := 536870912
-BOARD_FLASH_BLOCK_SIZE := 262144
