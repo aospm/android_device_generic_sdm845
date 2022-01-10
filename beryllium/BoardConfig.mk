283495390ed5f8f7f377baa65bcb9b1a77469e8b
Alexander Martinz <amartinz@shiftphones.com>
2022-01-10 17:05:19 +0100

    sdm845: commonize BoardConfigs
    As beryllium and enchilada can already boot using the same images,
    unify the kernel and system image configuration.
    
    Change-Id: I2458e11959bbb88e91c233df7e6f613839d74bc3
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- beryllium/BoardConfig.mk
+++ beryllium/BoardConfig.mk
@@ -4,21 +4,3 @@ include device/generic/sdm845/BoardConfigCommon.mk
 # Board Information
 TARGET_BOOTLOADER_BOARD_NAME := beryllium
 TARGET_BOARD_PLATFORM := beryllium
-
-# Kernel/boot.img Configuration
-BOARD_KERNEL_BASE        := 0x00000000
-BOARD_KERNEL_PAGESIZE    := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET     := 0x01000000
-BOARD_KERNEL_CMDLINE     := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
-BOARD_KERNEL_CMDLINE     += androidboot.hardware=beryllium androidboot.boot_devices=soc@0/1d84000.ufshc
-BOARD_KERNEL_CMDLINE     += firmware_class.path=/vendor/firmware/ init=/init printk.devkmsg=on
-BOARD_KERNEL_CMDLINE     += deferred_probe_timeout=30
-BOARD_KERNEL_CMDLINE     += fw_devlink=permissive
- 
-# Image Configuration
-BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864 #64M
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 57453555712
-BOARD_VENDORIMAGE_PARTITION_SIZE := 536870912
-BOARD_FLASH_BLOCK_SIZE := 262144
--- enchilada/BoardConfig.mk
+++ enchilada/BoardConfig.mk
@@ -4,21 +4,3 @@ include device/generic/sdm845/BoardConfigCommon.mk
 # Board Information
 TARGET_BOOTLOADER_BOARD_NAME := enchilada
 TARGET_BOARD_PLATFORM := enchilada
-
-# Kernel/boot.img Configuration
-BOARD_KERNEL_BASE        := 0x00000000
-BOARD_KERNEL_PAGESIZE    := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET     := 0x01000000
-BOARD_KERNEL_CMDLINE     := earlycon earlyprintk console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
-BOARD_KERNEL_CMDLINE     += androidboot.hardware=enchilada androidboot.boot_devices=soc@0/1d84000.ufshc
-BOARD_KERNEL_CMDLINE     += firmware_class.path=/vendor/firmware/ init=/init printk.devkmsg=on
-BOARD_KERNEL_CMDLINE     += deferred_probe_timeout=30
-BOARD_KERNEL_CMDLINE     += fw_devlink=on selinux=0
-
-# Image Configuration
-BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864 #64M
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2998927360
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 118112366592
-BOARD_VENDORIMAGE_PARTITION_SIZE := 536870912
-BOARD_FLASH_BLOCK_SIZE := 262144
