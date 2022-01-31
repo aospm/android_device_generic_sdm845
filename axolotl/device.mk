aospm/android_device_generic_sdm845

commit 298ae955c9b865c790c6bba59a97e29103a2a049

Remove sdm845-userdebug build target
Remove the common sdm845-userdebug build target.
Move the common device configs to device-common.mk and
{beryllium,enchilada}/device.mk file.

Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

beryllium/device.mk
@@ -14,11 +14,15 @@
 # limitations under the License.
 #
 
+PRODUCT_COPY_FILES := \
+    $(LOCAL_PATH)/fstab.ramdisk:$(TARGET_COPY_OUT_RAMDISK)/fstab.beryllium \
+    $(LOCAL_PATH)/fstab.ramdisk:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.beryllium \
+    device/generic/sdm845/init.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.beryllium.rc \
+    device/generic/sdm845/init.common.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.beryllium.usb.rc \
+    device/generic/sdm845/common.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/beryllium.kl
+
 # Build generic Audio HAL
-PRODUCT_PACKAGES := audio.primary.$(TARGET_DEVICE)
+PRODUCT_PACKAGES := audio.primary.beryllium
 
 # Build generic lights HAL
-PRODUCT_PACKAGES += lights.$(TARGET_DEVICE)
-
-RAMDISK_FSTAB_TARGET := $(LOCAL_PATH)/fstab.ramdisk
-$(call inherit-product, device/generic/sdm845/sdm845/device.mk)
+PRODUCT_PACKAGES += lights.beryllium

device-common.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+# copied from crosshatch
+# setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
@@ -76,6 +80,17 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf \
     wificond
+
+PRODUCT_PACKAGES += \
+    pd-mapper \
+    qrtr-ns \
+    qrtr-cfg \
+    qrtr-lookup \
+    rmtfs \
+    tqftpserv
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/qcom/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qcom.rc
 
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
@@ -201,4 +216,6 @@ PRODUCT_COPY_FILES +=  \
 # Copy firmware files
 $(call inherit-product-if-exists, $(LOCAL_PATH)/firmware/device.mk)
 
-PRODUCT_SOONG_NAMESPACES += external/mesa3d
+PRODUCT_SOONG_NAMESPACES += \
+    device/generic/sdm845 \
+    external/mesa3d

enchilada/device.mk
@@ -14,11 +14,15 @@
 # limitations under the License.
 #
 
+PRODUCT_COPY_FILES := \
+    $(LOCAL_PATH)/fstab.ramdisk:$(TARGET_COPY_OUT_RAMDISK)/fstab.enchilada \
+    $(LOCAL_PATH)/fstab.ramdisk:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.enchilada \
+    device/generic/sdm845/init.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.enchilada.rc \
+    device/generic/sdm845/init.common.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.enchilada.usb.rc \
+    device/generic/sdm845/common.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/enchilada.kl
+
 # Build generic Audio HAL
-PRODUCT_PACKAGES := audio.primary.$(TARGET_DEVICE)
+PRODUCT_PACKAGES := audio.primary.enchilada
 
 # Build generic lights HAL
-PRODUCT_PACKAGES += lights.$(TARGET_DEVICE)
-
-RAMDISK_FSTAB_TARGET := $(LOCAL_PATH)/fstab.ramdisk
-$(call inherit-product, device/generic/sdm845/sdm845/device.mk)
+PRODUCT_PACKAGES += lights.enchilada

sdm845/device.mk
@@ -1,43 +1,0 @@
-#
-# Copyright (C) 2011 The Android Open-Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-PRODUCT_SOONG_NAMESPACES += \
-    device/generic/sdm845
-
-# copied from crosshatch
-# setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-
-# Allow to override ramdisk until we can produce fully generic builds
-RAMDISK_FSTAB_TARGET ?= $(LOCAL_PATH)/fstab.ramdisk
-
-PRODUCT_COPY_FILES := \
-    $(RAMDISK_FSTAB_TARGET):$(TARGET_COPY_OUT_RAMDISK)/fstab.sdm845 \
-    $(RAMDISK_FSTAB_TARGET):$(TARGET_COPY_OUT_VENDOR)/etc/fstab.sdm845 \
-    device/generic/sdm845/init.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.sdm845.rc \
-    device/generic/sdm845/init.common.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.sdm845.usb.rc \
-    device/generic/sdm845/common.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sdm845.kl
-
-PRODUCT_PACKAGES += \
-    pd-mapper \
-    qrtr-ns \
-    qrtr-cfg \
-    qrtr-lookup \
-    rmtfs \
-    tqftpserv
-
-PRODUCT_COPY_FILES += \
-    device/generic/sdm845/qcom/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qcom.rc

Android.mk
@@ -20,7 +20,7 @@
 # to only building on ARM if they include assembly. Individual makefiles
 # are responsible for having their own logic, for fine-grained control.
 
-ifneq ($(filter beryllium enchilada sdm845, $(TARGET_DEVICE)),)
+ifneq ($(filter beryllium enchilada, $(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 

AndroidProducts.mk
@@ -11,10 +11,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/beryllium.mk \
-    $(LOCAL_DIR)/enchilada.mk \
-    $(LOCAL_DIR)/sdm845.mk
+    $(LOCAL_DIR)/enchilada.mk
 
 COMMON_LUNCH_CHOICES := \
     beryllium-userdebug \
-    enchilada-userdebug \
-    sdm845-userdebug
+    enchilada-userdebug

BoardConfigCommon.mk
@@ -39,15 +39,12 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_CMDLINE     := console=ttyMSM0,115200n8 earlycon earlyprintk console=tty0 androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE     += androidboot.boot_devices=soc@0/1d84000.ufshc
-BOARD_KERNEL_CMDLINE     += androidboot.hardware=sdm845
 BOARD_KERNEL_CMDLINE     += firmware_class.path=/vendor/firmware/ init=/init printk.devkmsg=on
 BOARD_KERNEL_CMDLINE     += deferred_probe_timeout=30
 
 # Image Configuration
 BOARD_FLASH_BLOCK_SIZE := 262144
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 67108864 #64M
-BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 2998927360
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 57453555712
 BOARD_VENDORIMAGE_PARTITION_SIZE   := 536870912
 
 BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4

beryllium/BoardConfig.mk
@@ -4,3 +4,10 @@ include device/generic/sdm845/BoardConfigCommon.mk
 # Board Information
 TARGET_BOOTLOADER_BOARD_NAME := beryllium
 TARGET_BOARD_PLATFORM := beryllium
+
+# Kernel/boot.img Configuration
+BOARD_KERNEL_CMDLINE     += androidboot.hardware=beryllium
+
+# Image Configuration
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 57453555712

commit 488c2c012c30c92703c24ddbab70eaeda6bf4bcc

Fix tinyhal / audio configuration
tinyhal loads audio.$(getprop ro.product.device).xml
configuration file for audio mixer control and routing
changes. So copy audio.sdm845.xml accordingly.

Fixes: 298ae955c9b8 ("Remove sdm845-userdebug build target")
Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

axolotl/device.mk
@@ -17,6 +17,7 @@
 PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/fstab.ramdisk:$(TARGET_COPY_OUT_RAMDISK)/fstab.axolotl \
     $(LOCAL_PATH)/fstab.ramdisk:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.axolotl \
+    device/generic/sdm845/etc/audio.sdm845.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio.axolotl.xml \
     device/generic/sdm845/init.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.axolotl.rc \
     device/generic/sdm845/init.common.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.axolotl.usb.rc \
     device/generic/sdm845/common.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/axolotl.kl

beryllium/device.mk
@@ -17,6 +17,7 @@
 PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/fstab.ramdisk:$(TARGET_COPY_OUT_RAMDISK)/fstab.beryllium \
     $(LOCAL_PATH)/fstab.ramdisk:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.beryllium \
+    device/generic/sdm845/etc/audio.sdm845.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio.beryllium.xml \
     device/generic/sdm845/init.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.beryllium.rc \
     device/generic/sdm845/init.common.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.beryllium.usb.rc \
     device/generic/sdm845/common.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/beryllium.kl

device-common.mk
@@ -135,7 +135,6 @@ PRODUCT_PACKAGES += \
 # audio policy configuration
 USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/etc/audio.sdm845.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio.sdm845.xml \
     $(LOCAL_PATH)/etc/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/etc/audio_policy_configuration_bluetooth_legacy_hal.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_bluetooth_legacy_hal.xml \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \

enchilada/device.mk
@@ -17,6 +17,7 @@
 PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/fstab.ramdisk:$(TARGET_COPY_OUT_RAMDISK)/fstab.enchilada \
     $(LOCAL_PATH)/fstab.ramdisk:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.enchilada \
+    device/generic/sdm845/etc/audio.sdm845.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio.enchilada.xml \
     device/generic/sdm845/init.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.enchilada.rc \
     device/generic/sdm845/init.common.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.enchilada.usb.rc \
     device/generic/sdm845/common.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/enchilada.kl