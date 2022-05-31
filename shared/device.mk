9e572dad7a159b5c32fdcd7d5df9a3790d8e2be1
Caleb Connolly <caleb.connolly@linaro.org>
2022-05-31 18:49:53 +0100

    Enable bluetooth profiles with product.prop
    Copied from Pixel5 (redbull) device config.
    
    Test: BT mouse and BT speakers work.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    [caleb: form factor port]
    https://android-review.googlesource.com/c/device/linaro/dragonboard/+/2103025

--- shared/device.mk
+++ shared/device.mk
@@ -215,6 +215,8 @@ PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.software.device_admin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_admin.xml
 
+TARGET_PRODUCT_PROP := $(LOCAL_PATH)/product.prop
+
 # Copy firmware files
 $(call inherit-product-if-exists, $(LOCAL_PATH)/firmware/device.mk)
 
