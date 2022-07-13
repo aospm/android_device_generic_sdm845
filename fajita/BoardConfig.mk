a544ba727c3285c071ae355452360fbdb3c57cc7
sc-ah <aljoshua.hell@symblcrowd.de>
2022-07-13 13:53:15 +0200

    sdm845: add support for Oneplus6T

---
+++ fajita/BoardConfig.mk
@@ -0,0 +1,14 @@
+include device/generic/sdm845/shared/BoardConfig.mk
+
+# Copied from https://github.com/LineageOS/android_device_oneplus_sdm845-common/blob/lineage-16.0/BoardConfigCommon.mk
+# Board Information
+TARGET_BOOTLOADER_BOARD_NAME := fajita
+TARGET_BOARD_PLATFORM := fajita
+TARGET_SCREEN_DENSITY := 400
+
+# Kernel/boot.img Configuration
+BOARD_KERNEL_CMDLINE     += androidboot.hardware=fajita
+
+# Image Configuration
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 2998927360
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 57453555712
